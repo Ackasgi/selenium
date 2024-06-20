5e6f4451f6bf4bb33e4c8c62e9f03b0071586b5c
Ackasgi <hello.git.ghkdtlsdn2724@gmail.com>
2024-06-20 18:57:52 +0900

    추가

--- test.py
+++ test.py
@@ -1,20 +1,18 @@
-from selenium.webdriver.common.by import By
-from selenium import webdriver
 import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
 chrome_driver_path = 'C:/temp/chromedriver.exe'
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument(f"webdriver.chrome.driver={chrome_driver_path}")
 driver = webdriver.Chrome(options=chrome_options)
 url = 'http://www.tongdocc.co.kr/'
 driver.get(url)
-time.sleep(0.5)
-xpath = '//input[@value="닫기"]'
+xpath = "//h5[@class='golfu_btn']"
 driver.find_element("xpath", xpath).click()
-xpath = '//li[@class="loginpop"]'
-driver.find_element("xpath", xpath).click()
-time.sleep(1)
-xpath = 'li [@rel="tab2"]'
-driver.find_element("xpath", xpath).click()
-#xpath = '//input[@type="text"]'
-#text = '01023602724'
-#driver.find_element("xpath", xpath).send_keys(text)
+xpath = '//input[@type="text"]'
+text = '01024465660'
+driver.find_element("xpath", xpath).send_keys(text)
+# xpath = '//input[@value="닫기"]'
+# driver.find_element("xpath", xpath).click()
+
